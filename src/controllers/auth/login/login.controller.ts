ec699eafb4750936a1a32d58426a4832231d5b29
jesus chavez <jesus.chavez@kadsoft.io>
2021-12-13 07:14:46 +0800

    se empezaron a controlar los errores por try catch

--- src/controllers/auth/login/login.controller.ts
+++ src/controllers/auth/login/login.controller.ts
@@ -15,40 +15,63 @@ export class LoginController {
    @UseGuards(LocalAuthGuard)
     @Post('auth/login')
     async login(@Request() req, @Res() res: Response) {
-        return  res.status(HttpStatus.OK).json(
-            await this._authService.login(req.user),
-        )
+        try {
+            return  res.status(HttpStatus.OK).json(
+                await this._authService.login(req.user),
+            )
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: error.message,
+                mensaje: 'Error al autenticar usuario'
+            })  
+        }
     }
 
     @UseGuards(AuthGuard('jwt-refreshtoken'))
     @Post('auth/refreshtoken')
-    async refreshToken(@Request() req){
-        return await this._authService.login(req.user);
+    async refreshToken(@Request() req, @Res() res: Response){
+        try {
+            return await this._authService.login(req.user);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: error.message,
+                mensaje: 'Error al autenticar usuario'
+            })  
+        }
     }
 
     @Post('auth/google')
     async googleLogin(@Request() req, @Res() res: Response) {
-        
+        // obtenemos el id_token de usuario proporcionada por google
         const { id_token } = req.body;
 
         try{
+            // verificamos el token y desestructuramos el usuario
             const {nombre, avatar, correo} = await verificacionGoogle(id_token);
+            // verificamos si el usuario existe en la base de datos con el correo
             const usuario = await this._usuariosService.obtenerUsuario(correo);
+            // si el usuario no existe, creamos un nuevo usuario
             if(!usuario){
-                const data = { nombre, avatar, correo, password: ':p', google: true, estado: true
-                }
+                // creamos el usuario
+                const data = { nombre, avatar, correo, password: ':p', google: true, estado: true };
+                // guardamos el usuario
                 const usuario = await this._usuariosService.crearNuevoUsuario(data);
+                // creamos el token y retornamos la respuesta
                 return res.status(HttpStatus.OK).json({
                     usuario,
                     token: await this._authService.login(usuario)
                 })
             }
+            // si el usuario existe, retornamos el token y el usuario para que se pueda loguear
             if(usuario){
                 return res.status(HttpStatus.OK).json({
                     usuario,
                     token: await this._authService.login(usuario)
                 })
             }
+            // si el usuario {estado: false}, retornamos un error
             if(!usuario.estado){
                 return res.status(HttpStatus.FORBIDDEN).json({
                     ok: false,
@@ -56,9 +79,12 @@ export class LoginController {
                 })
             }
             return res.status(HttpStatus.OK).json({id_token})
-        }catch(e){
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                error: e.message
+        }catch(error){
+            // si ocurre un error, retornamos un error
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: error.message,
+                mensaje: 'Error al autenticar usuario'
             })
         }
     }
--- src/controllers/usuarios/usuarios.controller.ts
+++ src/controllers/usuarios/usuarios.controller.ts
@@ -18,60 +18,80 @@ export class UsuariosController {
 
     @Get()
     async obtenerUsuarios(@Res() res: Response, @Query('limite') limite: number = 5, @Query('desde') desde: number = 0) {
-        const usuarios = await this._usuarioService
-        .obtenerUsuarios( Number(desde), Number(limite) );
-        return res.status(HttpStatus.OK).json({ usuarios });
+        try {
+            const usuarios = await this._usuarioService
+            .obtenerUsuarios( Number(desde), Number(limite) );
+            return res.status(HttpStatus.OK).json({ usuarios });
+        }catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: error.message });
+        }
     }
 
     @Post('usuario')
     async obtenerUsuario(@Res() res: Response, @Body() body: any){
-        const usuario = await this._usuarioService.obtenerUsuario(body.correo);
-        return res.status(HttpStatus.OK).json({ usuario });
+        try {
+            const usuario = await this._usuarioService.obtenerUsuario(body.correo);
+            return res.status(HttpStatus.OK).json({ usuario });
+        }catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: error.message });
+        }
     }
     
     @Post('crear-usuario')
     async crearUsuario(@Res() res: Response, @Body() body: CrearUsuarioDTO ) {
-        // Numeros de vueltas por defecto 10
-        const salt = await bcrypt.genSalt();
-        // Encriptar en una sola via
-        body.password = await bcrypt.hash(body.password, salt);
-        // Extraemos el password para no mostrarlo como respuesta
-        const usuario = await this._usuarioService.crearNuevoUsuario(body);
-        // resgresamos el usuario creado
-        return res.status(HttpStatus.OK).json({
-            message: 'Usuario creado',
-            usuario
-        })
+        try {
+            // Numeros de vueltas por defecto 10
+            const salt = await bcrypt.genSalt();
+            // Encriptar en una sola via
+            body.password = await bcrypt.hash(body.password, salt);
+            // Extraemos el password para no mostrarlo como respuesta
+            const usuario = await this._usuarioService.crearNuevoUsuario(body);
+            // resgresamos el usuario creado
+            return res.status(HttpStatus.OK).json({
+                message: 'Usuario creado',
+                usuario
+            })
+        }catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: error.message });
+        }
     }
 
     @UseGuards(JwtAuthGuard)
     @Put('actualizar-usuario')
     async actualizarusuario(@Res() res: Response, @Body() body: ActualizarUsuarioDTO ){
-        // Si el password existe lo actualizamos
-        if( body.password ){
-            const salt = await bcrypt.genSalt();
-            body.password = await bcrypt.hash(body.password, salt);
+        try {
+            // Si el password existe lo actualizamos
+            if( body.password ){
+                const salt = await bcrypt.genSalt();
+                body.password = await bcrypt.hash(body.password, salt);
+            }
+            // Hacemos la peticion al servicio
+            const usuaioAcualizado = await this._usuarioService.actualizarUsuario(body.id, body);
+            // resgresamos el usuario actualizado
+            const { estado } = usuaioAcualizado
+            // Verificamos que el usuario que quiere actualizar se encuentra en estado true
+            return ( estado ) 
+            ? res.status(HttpStatus.OK).json({message: 'Usuario Actualizado', usuaioAcualizado})
+            : res.status(HttpStatus.BAD_REQUEST).json({ message: 'Usuario no encontrado' })  
+        }catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: error.message });
         }
-        // Hacemos la peticion al servicio
-        const usuaioAcualizado = await this._usuarioService.actualizarUsuario(body.id, body);
-        // resgresamos el usuario actualizado
-        const { estado } = usuaioAcualizado
-        // Verificamos que el usuario que quiere actualizar se encuentra en estado true
-        return ( estado ) 
-        ? res.status(HttpStatus.OK).json({message: 'Usuario Actualizado', usuaioAcualizado})
-        : res.status(HttpStatus.BAD_REQUEST).json({ message: 'Usuario no encontrado' })  
     }
     
     @UseGuards(JwtAuthGuard)
     @Delete('borrar-usuario')
     async borrarUsuario(@Res() res: Response, @Body() body: BorrarUsuarioDTO) {
-        // Esperamos que el servicio responda
-        const usarioBorrado = await this._usuarioService.borrarUsuario(body.id);
-        //extraemos el estado del usuario
-        const { estado } = usarioBorrado
-        // si el usuario es estado true borramos el usuario
-        return ( estado ) 
-        ? res.status(HttpStatus.OK).json({message: 'Usuario Borrado', usarioBorrado}) 
-        : res.status(HttpStatus.BAD_REQUEST).json({ message: 'Usuario no encontrado' })
+        try {
+            // Esperamos que el servicio responda
+            const usarioBorrado = await this._usuarioService.borrarUsuario(body.id);
+            //extraemos el estado del usuario
+            const { estado } = usarioBorrado
+            // si el usuario es estado true borramos el usuario
+            return ( estado ) 
+            ? res.status(HttpStatus.OK).json({message: 'Usuario Borrado', usarioBorrado}) 
+            : res.status(HttpStatus.BAD_REQUEST).json({ message: 'Usuario no encontrado' })
+        }catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: error.message });
+        }
     }
 }
