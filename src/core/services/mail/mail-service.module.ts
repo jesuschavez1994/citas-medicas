bcb684216ec3f9462166682199e9f4ce8978a96d
jesus chavez <jesus.chavez@kadsoft.io>
2022-01-08 22:21:51 +0800

    Se agrego la plantilla para el correo

--- src/core/services/mail/mail-service.module.ts
+++ src/core/services/mail/mail-service.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailService } from './mail.service';
+import { join } from 'path';
 
 @Module({
     imports: [
@@ -16,6 +17,13 @@ import { MailService } from './mail.service';
             },
             defaults: {
                 from: '"No Reply" <noreply@example.com>',
+            },
+            template: {
+                dir: join(__dirname, 'templates'),
+                adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+                options: {
+                  strict: true,
+                },
             },
         })
     ],
