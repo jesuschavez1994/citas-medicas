c0fb77aef1481cafdf341514ac12a3804cdbd65d
jesus chavez <jesus.chavez@kadsoft.io>
2022-02-08 09:21:11 +0800

    se hizo lalogica para que el medico cree el cliente, ya tenia la del usuario solo fue este nuevo cambio que se hizo

--- src/controllers/medico/medico.module.ts
+++ src/controllers/medico/medico.module.ts
@@ -5,14 +5,15 @@ import { EspecialidadesServiceModule } from 'src/core/services/especialidades/es
 import { MailServiceModule } from 'src/core/services/mail/mail-service.module';
 import { MedicoServiceModule } from 'src/core/services/medico/medico-service.module';
 import { UsuariosModule } from '../usuarios/usuarios.module';
-
+import {ClienteServiceModule} from '../../core/services/cliente/cliente-service.module'
 @Module({
     imports:[
         MongooseModule.forFeature([{ name: Client.name, schema: Clientchema }]),
         MedicoServiceModule,
         UsuariosModule,
         EspecialidadesServiceModule,
-        MailServiceModule
+        MailServiceModule,
+        ClienteServiceModule
     ]
 })
 export class MedicoModule {}
